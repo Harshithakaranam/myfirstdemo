ad7af443c13092ba757e2f7eb3b2fcf9001b97be
Harshithakaranam <102942966+Harshithakaranam@users.noreply.github.com>
2024-08-27 20:30:26 +0530

    Add files via upload

--- store.js
+++ store.js
@@ -1,17 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import imageReducer, { loadImagesFromStorage, loadImages } from './imageSlice';
+import authReducer, { loadUsersFromStorage } from './authSlice';
 
 const store = configureStore({
   reducer: {
     images: imageReducer,
+    auth: authReducer,
   },
 });
 
-// Load images from AsyncStorage when the app starts
 const loadInitialData = async () => {
   try {
     const images = await loadImagesFromStorage();
     store.dispatch(loadImages(images));
+    await store.dispatch(loadUsersFromStorage());
   } catch (error) {
     console.error('Failed to load initial data:', error);
   }
